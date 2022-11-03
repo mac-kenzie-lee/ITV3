576297700042be002889f83e2bcb0726b7a4cabc
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-03 13:21:44 -0700

    l-3 final

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image: './assets/images/socks_blue.jpg',
+            imgDesc: 'A photo of green socks',
+            url: 'http://www.google.com'
         }
     }
 })
