8d5b4864cde43985a07d30bcd87a58dba61039a2
Mackenzie Lee <lee.n.mackenzie@gmail.com>
2022-11-03 13:10:19 -0700

    Solution for l2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({ //CREATES VUE INSTANCE
+    data() {
+        return {
+            product: 'Socks',
+            description: '100% Wool and Acrylic blend. Knit on Venetian looms, high durability and protection from elements.'
+        }
+    }
+ })
